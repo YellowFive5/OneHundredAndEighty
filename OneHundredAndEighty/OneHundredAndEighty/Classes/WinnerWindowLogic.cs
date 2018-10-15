43af69c6b9e2c335683bcb66a78364af012aff7a
YellowFive5 <yellowfive555@yahoo.com>
2018-10-15 13:07:03 +0300

    WinnerWindow Added

--- OneHundredAndEighty/OneHundredAndEighty/Classes/WinnerWindowLogic.cs
+++ OneHundredAndEighty/OneHundredAndEighty/Classes/WinnerWindowLogic.cs
@@ -11,17 +11,19 @@ using System.Windows.Media.Animation;
 
 namespace OneHundredAndEighty
 {
-    public class WinnerWindowLogic
+    public class WinnerWindowLogic  //  Класс логики окна победителя
     {
-        MainWindow MainWindow = ((MainWindow)System.Windows.Application.Current.MainWindow);    //  Ссылка на главное окно для доступа к элементам
-        Windows.WinnerWindow WinnerWindow = new Windows.WinnerWindow();
+        MainWindow MainWindow = null;    //  Ссылка на главное окно, зануляем
+        Windows.WinnerWindow WinnerWindow = null;    //  Ссылка на окно победителя, зануляем
 
         public void ShowWinner(Player p)
         {
-            WinnerWindow.Owner = MainWindow;
+            MainWindow MainWindow = ((MainWindow)System.Windows.Application.Current.MainWindow);    //  Обновляем ссылку на главное окно
+            Windows.WinnerWindow WinnerWindow = new Windows.WinnerWindow(); //  Обновляем ссылку на окно победителя
+            WinnerWindow.Owner = MainWindow;    //  Прописываем владельца
 
-
-            WinnerWindow.ShowDialog();
+            WinnerWindow.WinnerName.Content = new StringBuilder().Append("[ ").Append(p.Name).Append(" ]").ToString() ;
+            WinnerWindow.ShowDialog();  //  Показываем диалоговое окно
         }
     }
 }
