bae2f5b7fb3a5c3c35c7170f14ee661928aafb00
Евгений Болдырев <yellowfive555@yahoo.com>
2020-08-31 21:38:27 +0300

    WebApi test add

---
+++ OneHundredAndEighty/OneHundredAndEightyCoreWebApi/Startup.cs
@@ -0,0 +1,41 @@
+#region Usings
+
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+#endregion
+
+namespace OneHundredAndEightyCoreWebApi
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRouting(); 
+            
+            app.Map("/ping", Ping);
+
+            // app.UseEndpoints(endpoints => { endpoints.MapGet("/", async context => { await context.Response.WriteAsync("Hello World!"); }); });
+        }
+
+        private void Ping(IApplicationBuilder app)
+        {
+            app.Run(async context => { await context.Response.WriteAsync("Pong !"); });
+        }
+    }
+}
